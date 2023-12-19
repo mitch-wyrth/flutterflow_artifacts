b741b0054db0775453be333392cc2adf25925449
mitch-wyrth <113925566+mitch-wyrth@users.noreply.github.com>
2023-12-19 12:28:13 -0500

    Add files via upload

---
+++ icons_all-20241219/icons_all_icons.dart
@@ -0,0 +1,65 @@
+// Place fonts/icons_all.ttf in your fonts/ directory and
+// add the following to your pubspec.yaml
+// flutter:
+//   fonts:
+//    - family: icons_all
+//      fonts:
+//       - asset: fonts/icons_all.ttf
+import 'package:flutter/widgets.dart';
+
+class Icons_all {
+  Icons_all._();
+
+  static const String _fontFamily = 'icons_all';
+
+  static const IconData heart_outlined = IconData(0xe900, fontFamily: _fontFamily);
+  static const IconData team_add = IconData(0xe914, fontFamily: _fontFamily);
+  static const IconData chevroncompactdown = IconData(0xe912, fontFamily: _fontFamily);
+  static const IconData chevroncompactup = IconData(0xe913, fontFamily: _fontFamily);
+  static const IconData repeat = IconData(0xe911, fontFamily: _fontFamily);
+  static const IconData hamburger = IconData(0xe90f, fontFamily: _fontFamily);
+  static const IconData person = IconData(0xe910, fontFamily: _fontFamily);
+  static const IconData circlefill = IconData(0xe90e, fontFamily: _fontFamily);
+  static const IconData upvote_outline_converted = IconData(0xe90d, fontFamily: _fontFamily);
+  static const IconData upvote = IconData(0xe90c, fontFamily: _fontFamily);
+  static const IconData chevronbackward = IconData(0xe90b, fontFamily: _fontFamily);
+  static const IconData question_colored = IconData(0xe90a, fontFamily: _fontFamily);
+  static const IconData chat_enter_active = IconData(0xe909, fontFamily: _fontFamily);
+  static const IconData chat_enter = IconData(0xe904, fontFamily: _fontFamily);
+  static const IconData waveform = IconData(0xe905, fontFamily: _fontFamily);
+  static const IconData wyrthchatbot = IconData(0xe906, fontFamily: _fontFamily);
+  static const IconData plus = IconData(0xe901, fontFamily: _fontFamily);
+  static const IconData arrowup = IconData(0xe902, fontFamily: _fontFamily);
+  static const IconData magnifyingglass = IconData(0xe903, fontFamily: _fontFamily);
+  static const IconData team = IconData(0xe915, fontFamily: _fontFamily);
+  static const IconData next = IconData(0xe916, fontFamily: _fontFamily);
+  static const IconData equality = IconData(0xe917, fontFamily: _fontFamily);
+  static const IconData leaf = IconData(0xe918, fontFamily: _fontFamily);
+  static const IconData badweather = IconData(0xe919, fontFamily: _fontFamily);
+  static const IconData co2 = IconData(0xe91a, fontFamily: _fontFamily);
+  static const IconData healthy = IconData(0xe91b, fontFamily: _fontFamily);
+  static const IconData flag_big_outline = IconData(0xe91c, fontFamily: _fontFamily);
+  static const IconData flag_big_solid = IconData(0xe91d, fontFamily: _fontFamily);
+  static const IconData Home_outline_2 = IconData(0xe91e, fontFamily: _fontFamily);
+  static const IconData home_solid = IconData(0xe91f, fontFamily: _fontFamily);
+  static const IconData flag_hollow = IconData(0xe920, fontFamily: _fontFamily);
+  static const IconData flag_solid = IconData(0xe921, fontFamily: _fontFamily);
+  static const IconData bolt_outline = IconData(0xe922, fontFamily: _fontFamily);
+  static const IconData bolt_solid = IconData(0xe923, fontFamily: _fontFamily);
+  static const IconData stats = IconData(0xe924, fontFamily: _fontFamily);
+  static const IconData checkmark_sharp = IconData(0xe925, fontFamily: _fontFamily);
+  static const IconData close_x = IconData(0xe926, fontFamily: _fontFamily);
+  static const IconData comment_detailed = IconData(0xe927, fontFamily: _fontFamily);
+  static const IconData comment_simple = IconData(0xe928, fontFamily: _fontFamily);
+  static const IconData enter_hollow = IconData(0xe929, fontFamily: _fontFamily);
+  static const IconData enter_solid = IconData(0xe92a, fontFamily: _fontFamily);
+  static const IconData heart_outline = IconData(0xe92b, fontFamily: _fontFamily);
+  static const IconData heart_solid = IconData(0xe92c, fontFamily: _fontFamily);
+  static const IconData info_sansserif = IconData(0xe92d, fontFamily: _fontFamily);
+  static const IconData info_serif = IconData(0xe92e, fontFamily: _fontFamily);
+  static const IconData share1 = IconData(0xe92f, fontFamily: _fontFamily);
+  static const IconData thumbsdown = IconData(0xe930, fontFamily: _fontFamily);
+  static const IconData thumbsup = IconData(0xe931, fontFamily: _fontFamily);
+  static const IconData bank = IconData(0xe932, fontFamily: _fontFamily);
+  static const IconData doc = IconData(0xe933, fontFamily: _fontFamily);
+}
